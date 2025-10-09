6a7e2990e7c5364c4bbd183e956693fe5273f0ee
MedeFacu <142817713+MedeFacu@users.noreply.github.com>
2025-10-09 10:39:43 -0600

    tokenTypes.js

--- src/js/tokenTypes.js
+++ src/js/tokenTypes.js
@@ -1,9 +1,12 @@
 export const TokenType = {
+  // Palabras reservadas
   KEYWORD: "KEYWORD",
+  TYPE: "TYPE",        
   ID: "ID",
   NUM: "NUM",
   OP: "OP",
   DELIM: "DELIM",
   COMMENT: "COMMENT",
+  EOF: "EOF", 
   ERROR: "ERROR"
 };
